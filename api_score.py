20ce522b23e0a59586ef7f570f54c758743adec0
Stiven <stiven.guillaume@gmail.com>
2025-06-04 10:09:56 +0200

    update port 10000

--- api_score.py
+++ api_score.py
@@ -82,4 +82,4 @@ def predict():
         return jsonify({'error': str(e)}), 400
 
 if __name__ == "__main__":
-    waitress.serve(app, host="0.0.0.0", port=5001)
+    waitress.serve(app, host="0.0.0.0", port=10000)
